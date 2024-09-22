ab463607ddea78f87955bba59e38ca52f1b46d3e
Yain aitHamid <yassinaithamid@gmail.com>
2024-09-22 20:47:05 +0100

    / adding to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     server: 'https://realestatedakhlabackend.onrender.com/',
     mapKey: '5f977ba5-85a2-475a-88f6-90510a5a706f',
     googleAuthClientId: '',
-    webSocketUrl: 'ws://realestatedakhlabackend.onrender.com'
+    webSocketUrl: 'ws://realestatedakhlabackend.onrender.com/'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: false,
   api: {
-    server: 'https://realestatedakhlabackend.onrender.com',
+    server: 'https://realestatedakhlabackend.onrender.com/',
     mapKey: '5f977ba5-85a2-475a-88f6-90510a5a706f',
     googleAuthClientId: '',
-    webSocketUrl: "ws://realestatedakhlabackend.onrender.com"
+    webSocketUrl: "ws://realestatedakhlabackend.onrender.com/"
   }
 };
 
